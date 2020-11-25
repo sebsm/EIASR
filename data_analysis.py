902bda07f3af9ba66fcdb723cf477f032e27ac7b
selvert <01102827@pw.edu.pl>
2020-11-25 22:55:32 +0100

    Init

--- data_analysis.py
+++ data_analysis.py
@@ -1,2 +1,45 @@
-#EIASR Project init
-#Face recognition
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+# img = cv2.imread('1.pgm')
+# #mg = cv2.imread('3.jpg')
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# faces = face_cascade.detectMultiScale(gray, 1.3,4)
+
+# for (x,y,w,h) in faces:
+#     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+#     roi_gray = gray[y:y+h, x:x+w]
+#     roi_color = img[y:y+h, x:x+w]
+#     eyes = eye_cascade.detectMultiScale(roi_gray)
+#     #for (ex,ey,ew,eh) in eyes:
+#     #    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+# cv2.imshow('img',img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#image = cv2.imread('3.jpg')
+image = cv2.imread('abba.png')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.CASCADE_SCALE_IMAGE
+)
+
+print ('Found {0} faces!'.format(len(faces)))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Faces found", image)
+cv2.waitKey(0)
